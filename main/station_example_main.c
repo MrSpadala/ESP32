982e8d9b8567546387395c31662301d7a7002423
Spadala <pietro.spa@gmail.com>
2020-05-09 18:49:11 +0200

    add code for long polling

--- main/station_example_main.c
+++ main/station_example_main.c
@@ -15,6 +15,7 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_http_client.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
@@ -35,11 +36,14 @@ static EventGroupHandle_t s_wifi_event_group;
  * - are we connected to the AP with an IP? */
 const int WIFI_CONNECTED_BIT = BIT0;
 
-static const char *TAG = "wifi station";
+static const char *TAG = "wifi app";
 
 static int s_retry_num = 0;
 
-static void event_handler(void* arg, esp_event_base_t event_base, 
+/**
+    Handle events regarding wifi connection and ip obtaining
+*/
+static void conn_event_handler(void* arg, esp_event_base_t event_base, 
                                 int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
@@ -57,10 +61,16 @@ static void event_handler(void* arg, esp_event_base_t event_base,
         ESP_LOGI(TAG, "got ip:%s",
                  ip4addr_ntoa(&event->ip_info.ip));
         s_retry_num = 0;
+        
+        // Sets bit to notify we have an IP assigned
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
 
+
+/**
+    Connects to the net via wifi. It returns when we have an IP
+*/
 void wifi_init_sta(void)
 {
     s_wifi_event_group = xEventGroupCreate();
@@ -72,8 +82,8 @@ void wifi_init_sta(void)
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &conn_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &conn_event_handler, NULL));
 
     wifi_config_t wifi_config = {
         .sta = {
@@ -85,10 +95,91 @@ void wifi_init_sta(void)
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
     ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+
+    // Wait for IP to be set
+    xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, true, true, portMAX_DELAY);
 }
+
+
+/**
+    Handles HTTP events
+*/
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // Write out data
+                printf("%.*s", evt->data_len, (char*)evt->data);
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            // Maybe to check what it does
+            esp_err_t err = ESP_OK;
+            //err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            break;
+    }
+    return ESP_OK;
+}
+
+
+
+static void http_request_task(void *pvParameters)
+{
+    // Configure HTTP client
+    esp_http_client_config_t config = {
+        .url = "http://192.168.1.8:5000/cazzo/300",    //response after 10 seconds
+        .event_handler = _http_event_handler,
+        .timeout_ms = 305000,   //HTTP timeout after 15 seconds
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    for (int i=0; i<10; i++) {
+        // Perform a GET
+        ESP_LOGI(TAG, "Request number %d", i+1);
+        esp_err_t err = esp_http_client_perform(client);
+        if (err == ESP_OK) {
+            ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+                    esp_http_client_get_status_code(client),
+                    esp_http_client_get_content_length(client));
+        } else {
+            ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+        }
+    }
+
+    // Free HTTP client
+    esp_http_client_cleanup(client);
+    // Free RTOS task resources
+    ESP_LOGI(TAG, "Finish http request task");
+    vTaskDelete(NULL);
+}
+
+
 
 void app_main(void)
 {
@@ -100,6 +191,10 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
     
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    ESP_LOGI(TAG, "wifi_init_sta started");
     wifi_init_sta();
+    ESP_LOGI(TAG, "wifi_init_sta finished");
+
+    // Creates HTTP task
+    xTaskCreate(&http_request_task, "http_request_task", 8192, NULL, 5, NULL);
 }
