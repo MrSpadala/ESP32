db14a508dda8cb11f3fca7440877c2af8a7a9002
Pietro Spadaccino <pietro.spa@gmail.com>
2022-08-01 14:45:29 +0200

    update to esp-idf 5.0

--- main/main.c
+++ main/main.c
@@ -1,24 +1,44 @@
 
 #include <string.h>
 #include <stdbool.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "driver/gpio.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_pm.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "esp_http_client.h"
+#include <stdint.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/queue.h>
+#include <freertos/event_groups.h>
+#include <driver/gpio.h>
+#include <esp_system.h>
+#include <esp_wifi.h>
+#include <esp_pm.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+#include <esp_http_client.h>
 
-#include "lwip/err.h"
-#include "lwip/sys.h"
+#include <lwip/err.h>
+#include <lwip/sys.h>
 
 #define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+
+#if CONFIG_ESP_WIFI_AUTH_OPEN
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+#elif CONFIG_ESP_WIFI_AUTH_WEP
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+#elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+#endif
 
 /* Leds */
 #define BLINK_GPIO CONFIG_BLINK_GPIO
@@ -26,25 +46,28 @@
 #define GPIO_BUTTON_2_INPUT 2
 #define GPIO_BUTTON_INPUT_MASK ((1ULL<<GPIO_BUTTON_1_INPUT) | (1ULL<<GPIO_BUTTON_2_INPUT))
 
-/* URLs */
-#define URL_DEFAULT "http://my-word-service.herokuapp.com"
-#define URL_BLINK "http://my-word-service.herokuapp.com/cazzo/28"
-#define URL_BUTTON_1 "http://my-word-service.herokuapp.com/figa/1"
-#define URL_BUTTON_2 "http://my-word-service.herokuapp.com/figa/2"
-
+/* Telegram stuff */
+static uint32_t offset = 0;  //update_id offset
+#define TG_BASE_URL "https://api.telegram.org/bot"CONFIG_TG_BOT_TOKEN"/"
+#define TG_GET_UPDATES TG_BASE_URL"getUpdates?timeout=58&offset="
+#define SIZEOF_TG_GET_UPDATES_FMT 128
+static char tg_get_updates_fmt[SIZEOF_TG_GET_UPDATES_FMT];
+#define TG_SEND_HEART TG_BASE_URL"sendMessage?chat_id="CONFIG_TG_TARGET_USERID"&text=heart"
+#define TG_SEND_CHICK TG_BASE_URL"sendMessage?chat_id="CONFIG_TG_TARGET_USERID"&text=chicken"
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
-const int WIFI_CONNECTED_BIT = BIT0;
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+static uint32_t s_retry_num = 0;
 static bool connected = false;
-static int s_retry_num = 0;   //number of connection tries 
 
 /* Event group to signal when we receive a positive response from the server */
 static EventGroupHandle_t s_response_event_group;
 const int RESPONSE_BIT = BIT0;
 
 /* Queue of button presses events */
-static xQueueHandle button_press_evt_queue = NULL;
+static QueueHandle_t button_press_evt_queue = NULL;
 // Struct containing information about the button press, set by the interrupt handler
 // It will set inside button_press_evt_queue
 typedef struct {
@@ -69,7 +92,7 @@ static const char *TAG = "wifi app";
 /**
     Handle events regarding wifi connection and ip obtaining
 */
-static void conn_event_handler(void* arg, esp_event_base_t event_base, 
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
@@ -77,20 +100,16 @@ static void conn_event_handler(void* arg, esp_event_base_t event_base,
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
         if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
             esp_wifi_connect();
-            connected = false;
-            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
         ESP_LOGI(TAG,"connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:%s",
-                 ip4addr_ntoa(&event->ip_info.ip));
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
-        
-        // Sets bit to notify we have an IP assigned
-        connected = true;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
@@ -101,34 +120,64 @@ static void conn_event_handler(void* arg, esp_event_base_t event_base,
 */
 void wifi_init_sta(void)
 {
-    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_netif_init());
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &conn_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &conn_event_handler, NULL));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
 
     wifi_config_t wifi_config = {
         .sta = {
             .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+         .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+         .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
         },
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
-    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
 
-    // Wait for IP to be set
-    xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, true, true, portMAX_DELAY);
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
 
-    // Set wifi power save
-    esp_wifi_set_ps(DEFAULT_PS_MODE);
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        connected = true;
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
 }
 
 
@@ -153,19 +202,25 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
             ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
             break;
         case HTTP_EVENT_ON_DATA:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
             if (!esp_http_client_is_chunked_response(evt->client)) {
                 // Write out data
                 data_len = evt->data_len;
                 data = (char*)evt->data;
-                printf("%.*s\n", data_len, data);
-                if (data_len > 0) {
-                    if (data[0] == 'Y') {
-                        // Positive response, notify event group
-                        xEventGroupSetBits(s_response_event_group, RESPONSE_BIT);
-                    }
+                ESP_LOGI(TAG, "Data received:");
+                ESP_LOGI(TAG, "%s", data);
+
+                // Get bot updates
+                int n_matches = sscanf(data, "{\"ok\":true,\"result\":[{\"update_id\":%u,\"message\":{\"message_id\":%u,\"from\":{\"id\":"CONFIG_TG_TARGET_USERID, &offset, NULL);
+                if (n_matches == 0) {
+                    ESP_LOGI(TAG, "No new update found");
+                } else if (n_matches == 1) {
+                    // update offset and signal positive response
+                    ESP_LOGI(TAG, "New update found");
+                    offset += 1;
+                    xEventGroupSetBits(s_response_event_group, RESPONSE_BIT);
                 } else {
-                    ESP_LOGD(TAG, "empty response received");
+                    ESP_LOGE(TAG, "sscanf found two or more matches. This should not happen");
                 }
             }
 
@@ -183,6 +238,9 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
                 ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
                 ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
             }
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGE(TAG, "Received HTTP redirect, not handled");
             break;
     }
     return ESP_OK;
@@ -209,12 +267,19 @@ static esp_http_client_handle_t init_client_http(char* url) {
 static void http_request_task(void *pvParameters)
 {
     esp_err_t err;
-    esp_http_client_handle_t client = init_client_http(URL_BLINK);
+    esp_http_client_handle_t client = init_client_http(TG_BASE_URL);
 
     // Loop long polling requests forever using the same connection
-    for (int i=0; i>=0; i++) {
+    uint32_t i = 0;
+    while (true) {
         // Perform a GET
         ESP_LOGI(TAG, "Request number %d", i+1);
+
+        // Format URL
+        memset(tg_get_updates_fmt, 0, SIZEOF_TG_GET_UPDATES_FMT);
+        sprintf(tg_get_updates_fmt, TG_GET_UPDATES"%u", offset);
+        esp_http_client_set_url(client, tg_get_updates_fmt);
+
         err = esp_http_client_perform(client);
         if (err == ESP_OK) {
             ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
@@ -223,9 +288,10 @@ static void http_request_task(void *pvParameters)
         } else {
             ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
             esp_http_client_cleanup(client);  //clean http client
-            client = init_client_http(URL_BLINK);  //init new http client
-            vTaskDelay(3000 / portTICK_PERIOD_MS);  //backoff time
+            client = init_client_http(TG_BASE_URL);  //init new http client
+            vTaskDelay(10000 / portTICK_PERIOD_MS);  //backoff time
         }
+        i++;
     }        
 }
 
@@ -254,11 +320,11 @@ static void button_press_task(void* arg)
     TickType_t last_tick = xTaskGetTickCount();
     press_info_t info;
     bool http_repeat = false;
-    esp_http_client_handle_t client = init_client_http(URL_DEFAULT);
+    esp_http_client_handle_t client = init_client_http(TG_BASE_URL);
 
     // Configure input pin to handle button press
     gpio_config_t io_conf = {
-        .intr_type = GPIO_PIN_INTR_POSEDGE,  //interrupt of rising edge
+        .intr_type = GPIO_INTR_POSEDGE,  //interrupt of rising edge
         .pin_bit_mask = GPIO_BUTTON_INPUT_MASK, //bit mask of the pins
         .mode = GPIO_MODE_INPUT,             //set as input mode
         .pull_down_en = 1,                     //enable pull-down mode
@@ -288,9 +354,9 @@ static void button_press_task(void* arg)
 
             // Check which button was pressed and change URL accordingly
             if (info.gpio_num == GPIO_BUTTON_1_INPUT) {
-                esp_http_client_set_url(client, URL_BUTTON_1);
+                esp_http_client_set_url(client, TG_SEND_HEART);
             } else if (info.gpio_num == GPIO_BUTTON_2_INPUT) {
-                esp_http_client_set_url(client, URL_BUTTON_2);
+                esp_http_client_set_url(client, TG_SEND_CHICK);
             } else {
                 ESP_LOGE("button task", "unexpected gpio num %d", info.gpio_num);
                 continue;
@@ -299,6 +365,8 @@ static void button_press_task(void* arg)
             gpio_set_level(BLINK_GPIO, 1);  //turn on status led
 
             // Send HTTP request
+            // With default settings this throws an error since we have no certificate.
+            // To disable cert verification go in menuconfig->components->ESP-TLS->Allow potentially insecure options->skip certificate check
             err = esp_http_client_perform(client);
             if (err == ESP_OK) {
                 ESP_LOGI("button task", "HTTP GET Status = %d, content_length = %d",
@@ -307,7 +375,7 @@ static void button_press_task(void* arg)
             } else {
                 ESP_LOGE("button task", "HTTP GET request failed: %s", esp_err_to_name(err));
                 esp_http_client_cleanup(client);  //cleanup client
-                client = init_client_http(URL_DEFAULT); //restart client
+                client = init_client_http(TG_BASE_URL); //restart client
                 vTaskDelay(3000 / portTICK_PERIOD_MS);  //backoff time
                 xQueueSendToFront(button_press_evt_queue, &info, 0); //put event again in queue
                 http_repeat = true;   //signal that the next event in queue should be repeated (skip timestamp check)
@@ -326,7 +394,7 @@ static void led_task(void *pvParameters)
 {
     // Set up LED pin
     ESP_LOGI("LED_task", "Setup leds and starting blink");
-    gpio_pad_select_gpio(CONFIG_BLINK_GPIO);
+    esp_rom_gpio_pad_select_gpio(CONFIG_BLINK_GPIO);
     gpio_set_direction(CONFIG_BLINK_GPIO, GPIO_MODE_OUTPUT);
 
     // Loop blink until wifi connection is successful
@@ -377,7 +445,7 @@ void app_main(void)
     // Initialize wifi connection, response, button press event groups
     s_wifi_event_group = xEventGroupCreate();
     s_response_event_group = xEventGroupCreate();
-    button_press_evt_queue = xQueueCreate(10, sizeof(press_info_t));
+    button_press_evt_queue = xQueueCreate(20, sizeof(press_info_t));
 
     // Start LED task
     xTaskCreate(&led_task, "led_task", 2048, NULL, 5, NULL);
